1e5e5ba02d873329b2c6beeef0087ea6121fd694
Naoki Mita <naoki.mita18@gmail.com>
2020-07-02 17:45:38 -0700

    Add components on App

--- world-clock/src/App.test.tsx
+++ world-clock/src/App.test.tsx
@@ -1,10 +1,37 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent, act } from "@testing-library/react";
 import App from "./App";
 
 describe("renders", () => {
   test("renders title", () => {
     const { getByText } = render(<App />);
     expect(getByText("World Clock App")).toBeInTheDocument();
+  });
+});
+
+describe("functions", () => {
+  test("able to add a time list", () => {
+    const { getByPlaceholderText, getByText, getByRole } = render(<App />);
+    const input = getByPlaceholderText("type country or city name");
+    fireEvent.change(input, { target: { value: "canada" } });
+    fireEvent.click(getByText("Vancouver, Canada"));
+    getByRole("cell", { name: "Vancouver, Canada" });
+  });
+  test("able to remove a time list", () => {
+    const { getByPlaceholderText, getByText, queryByRole } = render(<App />);
+    const input = getByPlaceholderText("type country or city name");
+    fireEvent.change(input, { target: { value: "canada" } });
+    fireEvent.click(getByText("Vancouver, Canada"));
+    fireEvent.click(getByText("remove"));
+    expect(queryByRole("cell", { name: "Vancouver, Canada" })).toBeNull();
+  });
+  test("update the local time by each seconds", async () => {
+    const { getByPlaceholderText, getByText, getByRole } = render(<App />);
+    const input = getByPlaceholderText("type country or city name");
+    fireEvent.change(input, { target: { value: "canada" } });
+    fireEvent.click(getByText("Vancouver, Canada"));
+    getByRole("cell", { name: "7/1/2020, 4:50:19 PM" });
+    await act(() => new Promise((r) => setTimeout(r, 3000)));
+    getByRole("cell", { name: "7/1/2020, 4:50:22 PM" });
   });
 });
--- world-clock/src/App.tsx
+++ world-clock/src/App.tsx
@@ -1,14 +1,32 @@
-import React, { FC } from "react";
+import React, { FC, useReducer, useEffect, useState } from "react";
 import "./App.css";
 import SearchBox from "./components/SearchBox";
 import TimeList from "./components/TimeList";
+import { timeListReducer } from "./reducers";
+import { TState, TArea } from "./model/timeList.model";
+
+const initialState: TState = {
+  timelist: [],
+};
 
 const App: FC = () => {
+  const [{ timelist }, dispatch] = useReducer(timeListReducer, initialState);
+  const [diff, setDiff] = useState(0);
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      const newTimes: TArea[] = timelist.map(({ name, timestamp }) => ({ name, timestamp: timestamp + 1 }));
+      if (newTimes.length > 0) dispatch({ type: "UPDATE", newTimes });
+      setDiff((prev) => prev + 1);
+    }, 1000);
+    return () => clearTimeout(timerId);
+  }, [timelist]);
+
   return (
     <div className='App'>
       <h1>World Clock App</h1>
-      <SearchBox />
-      <TimeList />
+      <SearchBox diff={diff} dispatch={dispatch} />
+      <TimeList timelist={timelist} dispatch={dispatch} />
     </div>
   );
 };

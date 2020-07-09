163cfc5a962ed877b68d28787d2422517f65ad00
Naoki Mita <naoki.mita18@gmail.com>
2020-07-08 17:03:49 -0700

    Apply mockAxios on App.test

--- world-clock/src/App.test.tsx
+++ world-clock/src/App.test.tsx
@@ -1,26 +1,34 @@
 import React from "react";
-import { render, fireEvent, act } from "@testing-library/react";
+import { render, fireEvent, wait } from "@testing-library/react";
 import App from "./App";
+import { successData } from "./test/timezonedbTestData";
+import mockAxios from "./__mocks__/axios";
+
+jest.mock("axios");
 
 describe("renders", () => {
-  test("renders title", () => {
+  test("renders title", async () => {
+    mockAxios.get.mockResolvedValue(successData);
     const { getByText } = render(<App />);
-    expect(getByText("World Clock App")).toBeInTheDocument();
+    await wait(() => expect(getByText("World Clock App")).toBeInTheDocument());
   });
 });
 
 describe("functions", () => {
-  test("able to add a time list", () => {
+  beforeAll(() => {
+    mockAxios.get.mockResolvedValue(successData);
+  });
+  test("able to add a time list", async () => {
     const { getByPlaceholderText, getByText, getByRole } = render(<App />);
     const input = getByPlaceholderText("type country or city name");
-    fireEvent.change(input, { target: { value: "canada" } });
+    await wait(() => fireEvent.change(input, { target: { value: "canada" } }));
     fireEvent.click(getByText("Vancouver, Canada"));
     getByRole("cell", { name: "Vancouver, Canada" });
   });
-  test("able to remove a time list", () => {
+  test("able to remove a time list", async () => {
     const { getByPlaceholderText, getByText, queryByRole } = render(<App />);
     const input = getByPlaceholderText("type country or city name");
-    fireEvent.change(input, { target: { value: "canada" } });
+    await wait(() => fireEvent.change(input, { target: { value: "canada" } }));
     fireEvent.click(getByText("Vancouver, Canada"));
     fireEvent.click(getByText("remove"));
     expect(queryByRole("cell", { name: "Vancouver, Canada" })).toBeNull();
@@ -28,10 +36,10 @@ describe("functions", () => {
   test("update the local time by each seconds", async () => {
     const { getByPlaceholderText, getByText, getByRole } = render(<App />);
     const input = getByPlaceholderText("type country or city name");
-    fireEvent.change(input, { target: { value: "canada" } });
+    await wait(() => fireEvent.change(input, { target: { value: "canada" } }));
     fireEvent.click(getByText("Vancouver, Canada"));
-    getByRole("cell", { name: "7/1/2020, 4:50:19 PM" });
-    await act(() => new Promise((r) => setTimeout(r, 3000)));
-    getByRole("cell", { name: "7/1/2020, 4:50:22 PM" });
+    getByRole("cell", { name: "7/3/2020, 3:29:19 PM" });
+    await wait(() => new Promise((r) => setTimeout(r, 3000)));
+    getByRole("cell", { name: "7/3/2020, 3:29:22 PM" });
   });
 });
